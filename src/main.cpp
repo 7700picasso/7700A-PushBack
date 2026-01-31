3618a1d377dc243c65d2223b2f3823978e34282d
7700BVRC <94393717+7700picasso@users.noreply.github.com>
2026-01-30 16:50:32 -0800

    h

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,12 @@ competition Competition;
 // define your global instances of motors and other devices here
 brain Brain;
 
-motor LF (PORT10, ratio6_1, true);
-motor LM(PORT18, ratio6_1, true);
-motor LB(PORT1, ratio6_1, true);
-motor RF(PORT14, ratio6_1, false);
-motor RM(PORT20, ratio6_1, false);
-motor RB(PORT2, ratio6_1, false);
+motor LM (PORT10, ratio6_1, true);
+motor LB(PORT18, ratio6_1, true);
+motor LF(PORT19, ratio6_1, true);
+motor RM(PORT14, ratio6_1, false);
+motor RB(PORT20, ratio6_1, false);
+motor RF(PORT15, ratio6_1, false);
 
 motor Intake(PORT13, ratio18_1, false);
 motor Conveyor(PORT16, ratio6_1, true);
